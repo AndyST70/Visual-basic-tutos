6a6b08bf160ab42bbd5301d2a6b647126e065680
Andres <3021778410101@ingenieria.usac.edu.gt>
2022-08-01 11:46:39 -0600

    Clase 1

---
+++ Clase 1/Proyecto Hola mundo/Proyecto Hola mundo/Module1.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+    Const PULGADA As Integer = 2.54
+    Sub Main()
+        Dim numero As Integer = 12
+        Dim numero2 As Integer
+        Dim numero3 As Integer
+        Dim Resultado As Integer = 0
+        numero2 = 1
+
+        resultado = numero + numero2
+
+        numero2 = 2
+        numero3 = 5
+        Dim letra As Char = "a"
+        Dim cadena As String = "Hola Carlos"
+        Dim a As Boolean = True
+        Dim ad As Double = 2.1
+        Console.WriteLine(resultado)
+        Console.ReadKey(True)
+    End Sub
+    Sub otroP()
+
+    End Sub
+
+
+End Module
---
+++ Clase 1/app1/app1/Form1.vb
@@ -0,0 +1,72 @@
+﻿Public Class Form1
+    Dim num1, num2, resultado As Integer
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        num1 = Val(TextBox1.Text)
+        num2 = Val(TextBox2.Text)
+        resultado = Val(TextBox3.Text)
+        resultado = Val(num1) + Val(num2)
+        TextBox3.Text = resultado
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        num1 = Val(TextBox1.Text)
+        num2 = Val(TextBox2.Text)
+        resultado = Val(TextBox3.Text)
+        resultado = Val(num1) * Val(num2)
+        TextBox3.Text = resultado
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim num11, num22, resultado2 As Double
+        num11 = Val(TextBox1.Text)
+        num22 = Val(TextBox2.Text)
+        resultado2 = Val(TextBox3.Text)
+        resultado2 = Val(num11) / Val(num22)
+        TextBox3.Text = resultado2
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If Val(Edad.Text) < 18 Then
+            lbl.Text = "Es menor de edad"
+        Else
+            lbl.Text = "Es mayor de edad"
+        End If
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim Numero1, numero2, resultpapita As Integer 'esto hace referencia a un número
+        Numero1 = Val(TextBox1.Text)
+        numero2 = Val(TextBox2.Text) ' Obtenga los datos
+        resultpapita = Val(TextBox4.Text)
+        resultpapita = Val(Numero1) + Val(numero2)
+        TextBox4.Text = resultpapita
+
+
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        num1 = Val(TextBox1.Text)
+        num2 = Val(TextBox2.Text)
+        resultado = Val(TextBox3.Text)
+        resultado = Val(num1) - Val(num2)
+        TextBox3.Text = resultado
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        Edad.Clear()
+
+
+    End Sub
+End Class
